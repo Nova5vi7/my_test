b85e70bd9e95b340e90862a198cfc1393d069dbd
Forest_Hub <anatolijk096@gmail.com>
2020-07-23 17:38:51 +0300

    add scrollnav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -51,3 +51,72 @@ if (navigationMenu) {
   menuToggleListener(navigationButtonOff, navigationMenu, menuOverlay);
   menuToggleListener(menuOverlay, navigationMenu, menuOverlay);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  // слушаем событие загрузки DOM
+  const content = document.querySelectorAll(".section-scrollnav"); //
+  const container = document.createElement("div");
+  container.classList.add("container-links");
+
+  content.forEach((section, i) => {
+    const scrollBtn = document.createElement("button");
+    const order = i + 1;
+
+    scrollBtn.classList.add("scroll-nav-button");
+    scrollBtn.innerHTML = i < 9 ? `0${order}` : order;
+    scrollBtn.setAttribute("data-index", i);
+    section.setAttribute("data-index", i);
+    container.appendChild(scrollBtn);
+  });
+
+  container.addEventListener("click", (event) => {
+    const dataIndex = event.target.getAttribute("data-index");
+    if (!dataIndex) return;
+
+    const prevActive = container.querySelector(".active");
+
+    if (prevActive) {
+      prevActive.classList.remove("active");
+    }
+
+    event.target.classList.add("active");
+
+    content[dataIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  });
+
+  document.body.appendChild(container);
+
+  const scrollHandler = () => {
+    const windowCenter = window.scrollY + window.innerHeight / 2;
+
+    content.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.getBoundingClientRect().height;
+      const sectionBottom = sectionTop + sectionHeight;
+
+      if (windowCenter > sectionTop && sectionBottom > windowCenter) {
+        const currentActive = container.querySelector(".active");
+        const currentSectionIndex = section.getAttribute("data-index");
+        if (section.classList.contains("dark-scrollnav")) {
+          container.classList.add("light");
+        } else {
+          container.classList.remove("light");
+        }
+
+        if (currentActive) {
+          currentActive.classList.remove("active");
+        }
+
+        container
+          .querySelector(`[data-index="${currentSectionIndex}"]`)
+          .classList.add("active");
+      }
+    });
+  };
+
+  scrollHandler();
+  window.addEventListener("scroll", scrollHandler);
+});
